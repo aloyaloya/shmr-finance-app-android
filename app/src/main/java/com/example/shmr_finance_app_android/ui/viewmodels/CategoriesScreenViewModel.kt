0990af95de3bec43b6be7282bba93c76345a6b01
Egor <egbugaj@gmail.com>
2025-06-12 20:13:23 +0600

    feat(categories): implement BalanceViewModel

---
+++ app/src/main/java/com/example/shmr_finance_app_android/ui/viewmodels/CategoriesScreenViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.shmr_finance_app_android.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.shmr_finance_app_android.data.model.domain.Category
+import com.example.shmr_finance_app_android.data.model.mockCategories
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+sealed interface CategoriesScreenState {
+    object Loading : CategoriesScreenState
+    data class Error(val message: String, val retryAction: () -> Unit) : CategoriesScreenState
+    object Empty : CategoriesScreenState
+    data class Success(val categories: List<Category>) : CategoriesScreenState
+}
+
+class CategoriesScreenViewModel : ViewModel() {
+    private val _screenState = MutableStateFlow<CategoriesScreenState>(CategoriesScreenState.Loading)
+    val screenState: StateFlow<CategoriesScreenState> = _screenState.asStateFlow()
+
+    private val _searchRequest = MutableStateFlow("")
+    val searchRequest: StateFlow<String> = _searchRequest
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        _screenState.value = CategoriesScreenState.Loading
+        viewModelScope.launch {
+            try {
+                val categories = mockCategories
+                _screenState.value = if (categories.isEmpty()) {
+                    CategoriesScreenState.Empty
+                } else {
+                    CategoriesScreenState.Success(
+                        categories = categories
+                    )
+                }
+            } catch (e: Exception) {
+                _screenState.value = CategoriesScreenState.Error(
+                    message = e.message ?: "Неизвестна ошибка",
+                    retryAction = { loadCategories() }
+                )
+            }
+        }
+    }
+
+    fun onChangeSearchRequest(request: String) {
+        _searchRequest.value = request
+    }
+}
