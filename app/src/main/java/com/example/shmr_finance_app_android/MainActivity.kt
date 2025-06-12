bd663d1400f109240cde039f12885ae60a1c6b55
Egor <egbugaj@gmail.com>
2025-06-12 13:38:51 +0600

    refactor(main): assemble navigation components in MainActivity

--- app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.shmr_finance_app_android.navigation.AppNavHost
+import com.example.shmr_finance_app_android.navigation.RootScreen
+import com.example.shmr_finance_app_android.ui.navigation.BottomNavigationBar
+import com.example.shmr_finance_app_android.ui.navigation.CustomTopBar
 import com.example.shmr_finance_app_android.ui.theme.ShmrfinanceappandroidTheme
+import com.example.shmr_finance_app_android.ui.viewmodels.TopBarViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShmrfinanceappandroidTheme(darkTheme = false, dynamicColor = false) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val topBarViewModel: TopBarViewModel = viewModel()
+                val navController = rememberNavController()
+                val topBarState by topBarViewModel.state.collectAsState()
+
+                Scaffold(
+                    topBar = {
+                        CustomTopBar(
+                            state = topBarState,
+                            onBack = {
+                                navController.popBackStack()
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomNavigationBar(
+                            navController = navController,
+                            screens = RootScreen.screens
+                        )
+                    }
+                ) { innerPadding ->
+                    AppNavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        topBarViewModel = topBarViewModel,
+                        navController = navController
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShmrfinanceappandroidTheme {
-        Greeting("Android")
     }
 }
