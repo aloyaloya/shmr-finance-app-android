5a5bc154b7aad609af02a59c047b12e5829143ee
Egor <egbugaj@gmail.com>
2025-06-12 13:51:25 +0600

    chore(data): add mock expenses data

---
+++ app/src/main/java/com/example/shmr_finance_app_android/data/model/mockData.kt
@@ -0,0 +1,63 @@
+package com.example.shmr_finance_app_android.data.model
+
+import com.example.shmr_finance_app_android.data.model.domain.Expense
+
+val mockExpenses = listOf(
+    Expense(
+        id = "0",
+        title = "Аренда квартиры",
+        amount = 100000,
+        currency = "₽",
+        emoji = "🏡"
+    ),
+    Expense(
+        id = "1",
+        title = "Одежда",
+        amount = 100000,
+        currency = "₽",
+        emoji = "👗"
+    ),
+    Expense(
+        id = "2",
+        title = "На собачку",
+        author = "Джек",
+        amount = 100000,
+        currency = "₽",
+        emoji = "🐶"
+    ),
+    Expense(
+        id = "3",
+        title = "На собачку",
+        author = "Энни",
+        amount = 100000,
+        currency = "₽",
+        emoji = "🐶"
+    ),
+    Expense(
+        id = "4",
+        title = "Ремонт квартиры",
+        amount = 100000,
+        currency = "₽"
+    ),
+    Expense(
+        id = "5",
+        title = "Продукты",
+        amount = 100000,
+        currency = "₽",
+        emoji = "🍭"
+    ),
+    Expense(
+        id = "6",
+        title = "Спортзал",
+        amount = 100000,
+        currency = "₽",
+        emoji = "🏋️‍♂️"
+    ),
+    Expense(
+        id = "7",
+        title = "Медицина",
+        amount = 100000,
+        currency = "₽",
+        emoji = "💊"
+    ),
+)
