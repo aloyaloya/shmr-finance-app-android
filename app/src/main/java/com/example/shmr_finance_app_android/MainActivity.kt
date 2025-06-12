57ad184b4f2268a94b5f3f529b89a2a5016096f4
Egor <egbugaj@gmail.com>
2025-06-12 13:31:07 +0600

    refactor(theme): apply new colors to MaterialTheme

--- app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ShmrfinanceappandroidTheme {
+            ShmrfinanceappandroidTheme(darkTheme = false, dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
