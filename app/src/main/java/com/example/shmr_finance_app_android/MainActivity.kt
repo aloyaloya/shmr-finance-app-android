a3f58ee69cdc2069f0320649fecab7cbd644bd7c
Egor <egbugaj@gmail.com>
2025-06-13 19:25:27 +0600

    feat(splash): implement animated splash screen

--- app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.shmr_finance_app_android.ui.screens.main_screen.MainScreen
 import com.example.shmr_finance_app_android.ui.theme.ShmrfinanceappandroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             ShmrfinanceappandroidTheme(darkTheme = false, dynamicColor = false) {
