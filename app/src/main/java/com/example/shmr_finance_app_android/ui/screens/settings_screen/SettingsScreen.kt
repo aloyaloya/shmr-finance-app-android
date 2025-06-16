cbc76ee59adf477baa27cb6310928397f49fb186
Egor <egbugaj@gmail.com>
2025-06-16 20:38:59 +0600

    refactor(ui): replace SettingsOptionCard with ListItemCard

--- app/src/main/java/com/example/shmr_finance_app_android/ui/screens/settings_screen/SettingsScreen.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/ui/screens/settings_screen/SettingsScreen.kt
@@ -1,7 +1,9 @@
 package com.example.shmr_finance_app_android.ui.screens.settings_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -9,9 +11,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shmr_finance_app_android.R
-import com.example.shmr_finance_app_android.ui.screens.settings_screen.components.SettingsOptionCard
+import com.example.shmr_finance_app_android.data.model.ui.ListItem
+import com.example.shmr_finance_app_android.data.model.ui.MainContent
+import com.example.shmr_finance_app_android.ui.components.ListItemCard
 import com.example.shmr_finance_app_android.ui.screens.settings_screen.components.ThemeSwitcherOptionCard
 import com.example.shmr_finance_app_android.ui.viewmodels.SettingsScreenViewModel
 
@@ -32,9 +37,12 @@ fun SettingsScreen(
                 )
             }
             items(optionsItems) { option ->
-                SettingsOptionCard(
-                    title = option.title,
-                    onClick = { } // Открытие опции настроек
+                ListItemCard(
+                    modifier = Modifier
+                        .clickable { } // Переход пока не понятно куда
+                        .height(56.dp),
+                    item = ListItem(content = MainContent(title = option.title)),
+                    trailIcon = R.drawable.ic_filled_arrow_right
                 )
             }
         }
