e06eea42ab3f6565a82a4b0b005c06e253d059df
Egor <egbugaj@gmail.com>
2025-06-20 00:01:23 +0600

    build(deps): integrate Detekt into project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("com.google.dagger.hilt.android")
     id("kotlin-kapt")
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -39,6 +40,10 @@ android {
     buildFeatures {
         compose = true
     }
+}
+
+detekt {
+    buildUponDefaultConfig = true
 }
 
 dependencies {
