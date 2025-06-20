de4506cec6e2962fd936d8630583d31397f75c7b
Egor <egbugaj@gmail.com>
2025-06-20 19:45:20 +0600

    refactor(mock): delete mock data from 1st lab

--- app/src/main/java/com/example/shmr_finance_app_android/domain/model/Balance.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shmr_finance_app_android.domain.model
-
-data class Balance(
-    val name: String,
-    val balance: String,
-    val currency: String
-) {
-    val balanceFormatted: String get() = "$balance $currency"
-}
--- app/src/main/java/com/example/shmr_finance_app_android/domain/model/Option.kt
+++
@@ -1,5 +0,0 @@
-package com.example.shmr_finance_app_android.domain.model
-
-data class Option( // честно, пока вообще не знаю как настройки оформить
-    val title: String
-)
