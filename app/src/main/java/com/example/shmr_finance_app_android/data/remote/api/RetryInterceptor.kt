4087d2b2b1c8451258241451209f2ba961cc9883
Egor <egbugaj@gmail.com>
2025-06-26 15:52:37 +0600

    refactor(network): improve RetryInterceptor logic

--- app/src/main/java/com/example/shmr_finance_app_android/data/remote/api/RetryInterceptor.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/data/remote/api/RetryInterceptor.kt
@@ -1,11 +1,13 @@
 package com.example.shmr_finance_app_android.data.remote.api
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class RetryInterceptor(
     private val maxRetries: Int = 3,
-    private val retryDelayMillis: Long = 1000L
+    private val retryDelayMillis: Long = 2000L
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
@@ -14,8 +16,12 @@ class RetryInterceptor(
         var tryCount = 0
         while (!response.isSuccessful && response.code() == 500 && tryCount < maxRetries) {
             tryCount++
-            Thread.sleep(retryDelayMillis)
-            response.close() // закрываем предыдущий response
+            response.close()
+
+            runBlocking {
+                delay(retryDelayMillis)
+            }
+
             response = chain.proceed(request)
         }
 
