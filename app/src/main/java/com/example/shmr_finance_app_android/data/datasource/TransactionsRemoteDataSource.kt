b322c17f6b25fd2c6768f5fb030236219a00b742
Egor <egbugaj@gmail.com>
2025-06-27 17:45:32 +0600

    docs(datasource): add KDoc for data sources classes

--- app/src/main/java/com/example/shmr_finance_app_android/data/datasource/TransactionsRemoteDataSource.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/data/datasource/TransactionsRemoteDataSource.kt
@@ -5,6 +5,10 @@ import com.example.shmr_finance_app_android.data.remote.api.FinanceApiService
 import com.example.shmr_finance_app_android.data.remote.mapper.TransactionsRemoteMapper
 import javax.inject.Inject
 
+/**
+ * Отвечает за получение данных транзакций из remote-источника (API).
+ * Определяет контракт для работы с данными транзакций без привязки к конкретной реализации.
+ */
 interface TransactionsRemoteDataSource {
     suspend fun getTransactionsByPeriod(
         accountId: Int,
@@ -13,11 +17,32 @@ interface TransactionsRemoteDataSource {
     ): List<TransactionDTO>
 }
 
+/**
+ * Реализация [TransactionsRemoteDataSource], отвечающая за:
+ * - Запрос данных категорий
+ * - Преобразование сетевых моделей в DTO
+ *
+ * @param api - сервис для работы с API [FinanceApiService]
+ * @param mapper - маппер для преобразования сетевых моделей в DTO [TransactionsRemoteMapper]
+ */
 internal class TransactionsRemoteDataSourceImpl @Inject constructor(
     private val api: FinanceApiService,
     private val mapper: TransactionsRemoteMapper
 ) : TransactionsRemoteDataSource {
 
+    /**
+     * Реализация получения данных транзакций по периоду:
+     * 1. Запрашивает данные через API
+     * 2. Преобразует результат в DTO
+     * 3. Возвращает готовый список DTO
+     *
+     * @param accountId - ID аккаунта
+     * @param startDate - дата начала периода необходимых транзакций
+     * (по умолчанию - начало текущего месяца)
+     * @param endDate - дата конца периода необходимых транзакций
+     * (по умолчанию - конец текущего месяца)
+     * @return Список [TransactionDTO] - список преобразованных DTO данных транзакций
+     */
     override suspend fun getTransactionsByPeriod(
         accountId: Int,
         startDate: String?,
