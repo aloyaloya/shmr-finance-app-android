e06cd6c5f4c39552deedc1dce4f7bb4d8312a9c6
Egor <egbugaj@gmail.com>
2025-07-09 21:56:45 +0600

    refactor(app): migrate App/MainActivity DI from Hilt to Dagger

--- app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/MainActivity.kt
@@ -2,22 +2,34 @@ package com.example.shmr_finance_app_android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.shmr_finance_app_android.core.di.LocalActivityComponent
 import com.example.shmr_finance_app_android.presentation.feature.main.screen.MainScreen
 import com.example.shmr_finance_app_android.presentation.shared.theme.ShmrfinanceappandroidTheme
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val activityComponent by lazy {
+        (application as App).appComponent
+            .activityComponent()
+            .create()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
-            ShmrfinanceappandroidTheme(darkTheme = false, dynamicColor = false) {
-                MainScreen()
+            CompositionLocalProvider(
+                LocalActivity provides this,
+                LocalActivityComponent provides activityComponent
+            ) {
+                ShmrfinanceappandroidTheme(darkTheme = false, dynamicColor = false) {
+                    MainScreen()
+                }
             }
         }
     }
