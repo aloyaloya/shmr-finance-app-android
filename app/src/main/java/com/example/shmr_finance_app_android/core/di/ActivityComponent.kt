00dd4a90a17c28f78ebbd769ea26968c3af50549
Egor <egbugaj@gmail.com>
2025-07-09 21:39:38 +0600

    feat(di): create ActivityComponent with ViewModel provisioning

---
+++ app/src/main/java/com/example/shmr_finance_app_android/core/di/ActivityComponent.kt
@@ -0,0 +1,21 @@
+package com.example.shmr_finance_app_android.core.di
+
+import androidx.lifecycle.ViewModelProvider
+import dagger.Subcomponent
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
+
+@ActivityScope
+@Subcomponent(modules = [ViewModelModule::class])
+interface ActivityComponent {
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): ActivityComponent
+    }
+
+    fun viewModelProvider(): ViewModelProvider.Factory
+}
