57ff27ea795202678c09d49a57deff2ad2b3d068
Egor <egbugaj@gmail.com>
2025-07-09 21:39:53 +0600

    feat(di): create root AppComponent with singleton scope and module bindings

---
+++ app/src/main/java/com/example/shmr_finance_app_android/core/di/AppComponent.kt
@@ -0,0 +1,30 @@
+package com.example.shmr_finance_app_android.core.di
+
+import android.app.Application
+import com.example.shmr_finance_app_android.App
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AppModule::class,
+        NetworkModule::class,
+        RepositoryModule::class,
+        DataSourceModule::class,
+        ViewModelModule::class
+    ]
+)
+interface AppComponent {
+    fun inject(app: App)
+    fun activityComponent(): ActivityComponent.Factory
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+        fun appModule(appModule: AppModule): Builder
+        fun build(): AppComponent
+    }
+}
