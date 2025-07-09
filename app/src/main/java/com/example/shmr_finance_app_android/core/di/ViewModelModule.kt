d580059a87680f57278a3ba108c93d0adcd1c75f
Egor <egbugaj@gmail.com>
2025-07-09 21:50:14 +0600

    feat(di): register all ViewModels in centralized ViewModelModule

---
+++ app/src/main/java/com/example/shmr_finance_app_android/core/di/ViewModelModule.kt
@@ -0,0 +1,69 @@
+package com.example.shmr_finance_app_android.core.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.shmr_finance_app_android.presentation.feature.balance.viewmodel.BalanceScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.balance_edit.viewmodel.BalanceEditScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.categories.viewmodel.CategoriesScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.expenses.viewmodel.ExpensesScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.history.viewmodel.HistoryScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.incomes.viewmodel.IncomeScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.main.viewmodel.MainScreenViewModel
+import com.example.shmr_finance_app_android.presentation.feature.settings.viewmodel.SettingsScreenViewModel
+import dagger.Binds
+import dagger.MapKey
+import dagger.Module
+import dagger.multibindings.IntoMap
+import kotlin.reflect.KClass
+
+@Module
+interface ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(BalanceScreenViewModel::class)
+    fun bindBalanceViewModel(vm: BalanceScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(BalanceEditScreenViewModel::class)
+    fun bindBalanceEditViewModel(vm: BalanceEditScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(CategoriesScreenViewModel::class)
+    fun bindCategoriesViewModel(vm: CategoriesScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ExpensesScreenViewModel::class)
+    fun bindExpensesViewModel(vm: ExpensesScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HistoryScreenViewModel::class)
+    fun bindHistoryViewModel(vm: HistoryScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(IncomeScreenViewModel::class)
+    fun bindIncomesViewModel(vm: IncomeScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainScreenViewModel::class)
+    fun bindMainViewModel(vm: MainScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SettingsScreenViewModel::class)
+    fun bindSettingsViewModel(vm: SettingsScreenViewModel): ViewModel
+
+    @Binds
+    fun bindViewModelFactory(factory: ViewModelProviderFactory): ViewModelProvider.Factory
+}
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
