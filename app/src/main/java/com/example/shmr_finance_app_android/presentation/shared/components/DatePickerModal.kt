dfa0e1216eece5712b0ed79c50ca70bf244e83ea
Egor <egbugaj@gmail.com>
2025-07-11 12:58:49 +0600

    refactor(ui): move DatePickerModal to shared

--- app/src/main/java/com/example/shmr_finance_app_android/presentation/shared/components/DatePickerModal.kt
+++ app/src/main/java/com/example/shmr_finance_app_android/presentation/shared/components/DatePickerModal.kt
@@ -1,4 +1,4 @@
-package com.example.shmr_finance_app_android.presentation.feature.history.component
+package com.example.shmr_finance_app_android.presentation.shared.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
